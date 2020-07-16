689d914bb706d1da660849528aa573bbf5f29d3b
Dave Guymon <davidwguymon@gmail.com>
2020-07-15 18:31:15 -0600

    Update stock_picker.rb

--- ruby_programming/stock_picker.rb
+++ ruby_programming/stock_picker.rb
@@ -1,20 +1,19 @@
-def stock_picker(stocks)
+def stock_picker(prices)
   day_to_buy = 0
   day_to_sell = 0
   profit = 0
 
-  stocks.each_with_index do |price1, idx1|
-    stocks.each_with_index do |price2, idx2|
-      if idx1 < idx2 && (price2 - price1) > profit
-        day_to_buy = idx1
-        day_to_sell = idx2
-        profit = price2 - price1
+  while day_to_buy < prices.length
+    day_to_sell = day_to_buy + 1
+    while day_to_sell < prices.length
+      if prices[day_to_sell] - prices[day_to_buy] > profit
+        optimal_days = Array[day_to_buy, day_to_sell]
+        profit = prices[day_to_sell] - prices[day_to_buy]
       end
+      day_to_sell += 1
     end
+    day_to_buy += 1
   end
 
-  puts "Buy on day #{day_to_buy}; Sell on day #{day_to_sell}; Your profit will be $#{profit}!"
-
+  optimal_days
 end
-
-stock_picker([17,3,6,9,15,8,6,1,10])
